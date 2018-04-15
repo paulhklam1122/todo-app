862aa37f7e23cee51eda222bb96c264dd21f7518
Paul Lam <paulhklam1122@gmail.com>
2018-04-15 11:28:37 -0700

    forgot to add project files to be git pushed

---
+++ src/components/Root.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import App from './App'
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <Router>
+      <Route path='/:filter?' component={App} />
+    </Router>
+  </Provider>
+)
+Root.propTypes = {
+  store: PropTypes.oneOfType([
+    PropTypes.func.isRequired,
+    PropTypes.object.isRequired
+  ]).isRequired
+}
+
+export default Root
---
+++ src/configureStore.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import todoApp from './reducers'
+
+const configureStore = () => {
+  const middlewares = [thunk]
+
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(createLogger())
+  }
+
+  return createStore(
+    todoApp,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middlewares)
+  )
+}
+
+export default configureStore
